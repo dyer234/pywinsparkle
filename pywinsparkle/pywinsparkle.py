173e6961b68a094d67a42782b736d9296a8fa8e7
Daniel Dyer <dyer234@gmail.com>
2017-02-14 08:05:54 -0800

    first commit of winsparkle wrapper

---
+++ pywinsparkle/pywinsparkle.py
@@ -0,0 +1,242 @@
+""" A wrapper for the WinSparkle project
+
+"""
+from ctypes import *
+import os
+
+THIS_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+DLL_DIRECTORY = os.path.join(THIS_DIRECTORY, "libs")
+DLL_FILE = os.path.join(DLL_DIRECTORY, "WinSparkle")
+
+dll = cdll.LoadLibrary(DLL_FILE)
+
+
+def win_sparkle_init():
+    """ Starts WinSparkle.
+
+    If WinSparkle is configured to check for updates on startup, proceeds
+    to perform the check. You should only call this function when your app
+    is initialized and shows its main window.
+
+    @note This call doesn't block and returns almost immediately. If an
+          update is available, the respective UI is shown later from a separate
+          thread.
+
+    :return:
+    """
+    dll.win_sparkle_init.restype = None
+    dll.win_sparkle_init()
+
+
+def win_sparkle_cleanup():
+    """ Cleans up after WinSparkle.
+
+    Should be called by the app when it's shutting down. Cancels any
+    pending Sparkle operations and shuts down its helper threads.
+
+    :return:
+    """
+
+    dll.win_sparkle_cleanup.restype = None
+    dll.win_sparkle_cleanup()
+
+
+def win_sparkle_set_lang(language):
+    """ Sets UI language from its Win32 LANGID code.
+
+    This function must be called before win_sparkle_init().
+    :param language: Language code (LANGID) as created by the MAKELANGID macro
+                or returned by e.g. ::GetThreadUILanguage()
+    :return:
+    """
+    # TODO
+    raise Exception("Not Implemented")
+
+
+def win_sparkle_set_appcast_url(url):
+    """  Sets URL for the app's appcast. Only http and https schemes are supported.
+
+    If this function isn't called by the app, the URL is obtained from
+    Windows resource named "FeedURL" of type "APPCAST".
+
+    note: Always use HTTPS feeds, do not use unencrypted HTTP! This is
+          necessary to prevent both leaking user information and preventing
+          various MITM attacks.
+
+    :param url: URL of the appcast.
+    :return:
+    """
+
+    dll.win_sparkle_set_appcast_url.restype = None
+    dll.win_sparkle_set_appcast_url.argtypes = [c_char_p]
+    dll.win_sparkle_set_appcast_url(url.encode())
+
+
+def win_sparkle_set_app_details(company_name, app_name, app_version):
+    """ Sets application metadata.
+
+    Normally, these are taken from VERSIONINFO/StringFileInfo resources,
+    but if your application doesn't use them for some reason, using this
+    function is an alternative.
+
+    note  company_name and app_name are used to determine the location
+          of WinSparkle settings in registry.
+          (HKCU\Software\<company_name>\<app_name>\WinSparkle is used.)
+
+    :param company_name: Company name of the vendor.
+    :param app_name: Application name. This is both shown to the user
+                         and used in HTTP User-Agent header
+    :param app_version: Version of the app, as string (e.g. "1.2" or "1.2rc1").
+    :return:
+    """
+
+    dll.win_sparkle_set_app_details.restype = None
+    dll.win_sparkle_set_app_details.argtypes = [c_wchar_p, c_wchar_p, c_wchar_p]
+    dll.win_sparkle_set_app_details(company_name, app_name, app_version)
+
+
+def win_sparkle_set_app_build_version(build_number):
+    """ Sets application build version number.
+
+    This is the internal version number that is not normally shown to the user.
+    It can be used for finer granularity that official release versions, e.g. for
+    interim builds.
+
+    If this function is called, then the provided *build* number is used for comparing
+    versions; it is compared to the "version" attribute in the appcast and corresponds
+    to OS X Sparkle's CFBundleVersion handling. If used, then the appcast must
+    also contain the "shortVersionString" attribute with human-readable display
+    version string. The version passed to win_sparkle_set_app_details()
+    corresponds to this and is used for display.
+
+    :param build_number: the build number
+    :return:
+    """
+
+    dll.win_sparkle_set_app_build_version.restype = None
+    dll.win_sparkle_set_app_build_version.argtypes = [c_wchar_p]
+    dll.win_sparkle_set_app_build_version(build_number)
+
+
+def win_sparkle_set_registry_path(registry_path):
+    """ Set the registry path where settings will be stored.
+
+    Normally, these are stored in
+    "HKCU\Software\<company_name>\<app_name>\WinSparkle"
+    but if your application needs to store the data elsewhere for
+    some reason, using this function is an alternative.
+
+    Note that @a path is relative to HKCU/HKLM root and the root is not part
+    of it. For example:
+    example:
+    win_sparkle_set_registry_path("Software\\My App\\Updates");
+
+    :param registry_path: Registry path where settings will be stored.
+    :return:
+    """
+
+    dll.win_sparkle_set_registry_path.restype = None
+    dll.win_sparkle_set_registry_path.argtypes = [c_wchar_p]
+    dll.win_sparkle_set_registry_path(registry_path)
+
+
+def win_sparkle_set_automatic_check_for_updates(update_state):
+    """ Sets whether updates are checked automatically or only through a manual call.
+    If disabled, win_sparkle_check_update_with_ui() must be used explicitly.
+
+    :param update_state: 1 to have updates checked automatically, 0 otherwise
+    :return:
+    """
+
+    dll.win_sparkle_set_automatic_check_for_updates.restype = None
+    dll.win_sparkle_set_automatic_check_for_updates.argtypes = [c_int64]
+    dll.win_sparkle_set_automatic_check_for_updates(update_state)
+
+
+
+def win_sparkle_set_update_check_interval(interval):
+    """ Sets the automatic update interval.
+
+    :param interval: interval The interval in seconds between checks for updates.
+                     The minimum update interval is 3600 seconds (1 hour).
+    :return:
+    """
+
+    dll.win_sparkle_set_update_check_interval.restype = None
+    dll.win_sparkle_set_update_check_interval.argtypes = [c_int64]
+    dll.win_sparkle_set_update_check_interval(interval)
+
+
+def win_sparkle_get_update_check_interval():
+    """ Gets the time for the last update check.
+
+    Default value is -1, indicating that the update check has never run.
+
+    :return:
+    """
+
+    dll.win_sparkle_get_update_check_interval.restype = c_int64
+    dll.win_sparkle_get_update_check_interval.argtypes = None
+    result = dll.win_sparkle_get_update_check_interval()
+
+    return result
+
+
+def win_sparkle_check_update_with_ui():
+    """ Checks if an update is available, showing progress UI to the user.
+
+    Normally, WinSparkle checks for updates on startup and only shows its UI
+    when it finds an update. If the application disables this behavior, it
+    can hook this function to "Check for updates..." menu item.
+
+    When called, background thread is started to check for updates. A small
+    window is shown to let the user know the progress. If no update is found,
+    the user is told so. If there is an update, the usual "update available"
+    window is shown.
+
+    This function returns immediately.
+    :return:
+    """
+
+    dll.win_sparkle_check_update_with_ui.restype = None
+    dll.win_sparkle_check_update_with_ui()
+
+
+def win_sparkle_check_update_with_ui_and_install():
+    """ Checks if an update is available, showing progress UI to the user and
+    immediately installing the update if one is available.
+
+    This is useful for the case when users should almost always use the
+    newest version of your software. When called, WinSparkle will check for
+    updates showing a progress UI to the user. If an update is found the update
+    prompt will be skipped and the update will be installed immediately.
+
+    If your application expects to do something after checking for updates you
+    may wish to use win_sparkle_set_did_not_find_update_callback() and
+    win_sparkle_set_update_cancelled_callback().
+
+    :return:
+    """
+
+    dll.win_sparkle_check_update_with_ui_and_install.restype = None
+    dll.win_sparkle_check_update_with_ui_and_install()
+
+
+def win_sparkle_check_update_without_ui():
+    """ Checks if an update is available.
+
+    No progress UI is shown to the user when checking. If an update is
+    available, the usual "update available" window is shown; this function
+    is *not* completely UI-less.
+
+    Use with caution, it usually makes more sense to use the automatic update
+    checks on interval option or manual check with visible UI.
+
+    This function returns immediately.
+
+    :return:
+    """
+
+    dll.win_sparkle_check_update_without_ui.restype = None
+    dll.win_sparkle_check_update_without_ui()
